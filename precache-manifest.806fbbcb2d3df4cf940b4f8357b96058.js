0f379dce265ee3e966ca9a8d43738b18381de165
nandhini-murugesan <nandhu4222@gmail.com>
2020-06-24 12:14:45 +0530

    Updates

---
+++ precache-manifest.806fbbcb2d3df4cf940b4f8357b96058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8403eeb5d2c37e4e091fef556c433c",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "75bd24d7ec41273dd850",
+    "url": "/corona-tracker/static/css/main.69b1acf4.chunk.css"
+  },
+  {
+    "revision": "1565a0a5d5f63439a904",
+    "url": "/corona-tracker/static/js/2.fab3d5d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.fab3d5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bd24d7ec41273dd850",
+    "url": "/corona-tracker/static/js/main.9b57ba88.chunk.js"
+  },
+  {
+    "revision": "e6b58be38461e41709e6",
+    "url": "/corona-tracker/static/js/runtime-main.a1feae45.js"
+  }
+]);
